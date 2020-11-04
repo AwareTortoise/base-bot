8e2aa68e0723c63ee74688d40d4bf1c470bd9750
hugory101 <47349875+hugory101@users.noreply.github.com>
2020-11-04 09:17:15 +0000

    Update main.js

--- plugins/main.js
+++ plugins/main.js
@@ -17,62 +17,7 @@ class Main {
     }
 
     async _handleMessage(message) {
-        const words = message.content.trim().split(/\s+/g);
-        const command = words.shift();
-
-        if (command === '!mute') {
-            const member = message.mentions.members.first();
-
-            if (!member) {
-                return;
-            }
-
-            await member.setMute(true)
-                .catch(error => {
-                    message.channel.send(`Error: ${error.message}`);
-                });
-        }
-
-        if (command === '!unmute') {
-            const member = message.mentions.members.first();
-
-            if (!member) {
-                return;
-            }
-
-            await member.setMute(false)
-                .catch(error => {
-                    message.channel.send(`Error: ${error.message}`);
-                });
-        }
-
-        if (command === '!kick') {
-            const member = message.mentions.members.first();
-
-            if (!member) {
-                return;
-            }
-
-            await member.kick().catch(error => message.channel.send(`Error: ${error.message}`));
-        }
-
-        if (command === '!ban') {
-            const member = message.mentions.members.first();
-
-            if (!member) {
-                return;
-            }
-
-            await member.ban().catch(error => message.channel.send(`Error: ${error.message}`));
-        }
-
-        if (command === '!purge') {
-            try {
-                const fetched = await message.channel.fetchMessages({ limit: 100 });
-                message.channel.bulkDelete(fetched);
-            } catch (error) {
-                message.channel.send(`Error: ${error.message}`);
-            }
+        
         }
     }
 }
